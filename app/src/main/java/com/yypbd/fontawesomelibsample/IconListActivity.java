dbdc641c7bb7e5378dd6fa1682d642781ac2310e
YYPBD-MACMINI\yypbd <yypbd.work@gmail.com>
2020-03-30 11:20:30 +0900

    add: convert sample to kotlin

---
+++ app/src/main/java/com/yypbd/fontawesomelibsample/IconListActivity.java
@@ -0,0 +1,50 @@
+package com.yypbd.fontawesomelibsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yypbd.fontawesomelib.FontawesomeLib;
+
+import java.util.ArrayList;
+
+public class IconListActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewIcon;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icon_list);
+
+        recyclerViewIcon = findViewById(R.id.recyclerViewIcon);
+
+        ArrayList<IconItem> iconList = new ArrayList<>();
+
+        Intent intent = getIntent();
+        int type = intent.getIntExtra("font_type", 0);
+        FontawesomeLib.FontType fontType;
+        if (type == 0) {
+            fontType = FontawesomeLib.FontType.TYPE_SOLID;
+        }
+        else if (type == 1) {
+            fontType = FontawesomeLib.FontType.TYPE_REGULAR;
+        }
+        else {
+            fontType = FontawesomeLib.FontType.TYPE_BRAND;
+        }
+
+        for (String key : FontawesomeLib.getInstance().getCharMap(fontType).keySet()) {
+            iconList.add(new IconItem(key));
+        }
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerViewIcon.setLayoutManager(layoutManager);
+        mAdapter = new IconAdapter(fontType, iconList);
+        recyclerViewIcon.setAdapter(mAdapter);
+    }
+}
--- app/src/main/java/com/yypbd/fontawesomelibsample/MainActivity.kt
+++ app/src/main/java/com/yypbd/fontawesomelibsample/MainActivity.kt
@@ -1,12 +1,69 @@
 package com.yypbd.fontawesomelibsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.yypbd.fontawesomelib.FontawesomeLib
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FontawesomeLib.getInstance().init(
+            assets,
+            "fonts/fa-solid-900.ttf",
+            "fonts/fa-regular-400.ttf",
+            "fonts/fa-brands-400.ttf"
+        )
+
+        initUI()
+        initListener()
+    }
+
+    private fun initListener() {
+        buttonShowSolid.setOnClickListener(View.OnClickListener {
+            val intent = Intent(baseContext, IconListActivity::class.java)
+            intent.putExtra("font_type", 0)
+            startActivity(intent)
+        })
+        buttonShowRegular.setOnClickListener(View.OnClickListener {
+            val intent = Intent(baseContext, IconListActivity::class.java)
+            intent.putExtra("font_type", 1)
+            startActivity(intent)
+        })
+        buttonShowBrand.setOnClickListener(View.OnClickListener {
+            val intent = Intent(baseContext, IconListActivity::class.java)
+            intent.putExtra("font_type", 2)
+            startActivity(intent)
+        })
+        buttonTest.setOnClickListener(View.OnClickListener {
+            val text = FontawesomeLib.getInstance()
+                .getText(FontawesomeLib.FontType.TYPE_SOLID, "adjust")
+            Log.d("tag", text)
+            FontawesomeLib.getInstance()
+                .setIcon(FontawesomeLib.FontType.TYPE_SOLID, "adjust", buttonTest)
+            FontawesomeLib.getInstance()
+                .setIcon(FontawesomeLib.FontType.TYPE_SOLID, "atlas", textViewTest)
+
+            // https://github.com/devunwired/textdrawable
+            val textDrawable = TextDrawable(baseContext)
+            textDrawable.textSize = 50f
+            val tf =
+                FontawesomeLib.getInstance().getTypeface(FontawesomeLib.FontType.TYPE_SOLID)
+            textDrawable.setTypeface(tf)
+            textDrawable.setText(
+                FontawesomeLib.getInstance()
+                    .getText(FontawesomeLib.FontType.TYPE_SOLID, "asterisk")
+            )
+            imageViewTest.background = textDrawable
+        })
+    }
+
+    private fun initUI() {
     }
 }
